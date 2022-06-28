0ddaba2ee9a0f36a73e90364dde268037d9e7af5
Anton Motornenko <mtrn@Antons-MacBook-Pro-5.local>
2022-06-28 16:07:31 +0200

    updated main

--- src/main.cpp
+++ src/main.cpp
@@ -37,6 +37,7 @@
 #include "eo3.h"
 #include "eo1.h"
 #include "eoChiral.h"
+#include "eoCMF.h"
 #include "eoAZH.h"
 #include "eoHadron.h"
 #include "eoSmash.h"
@@ -296,6 +297,8 @@ int main(int argc, char **argv) {
   eos = new EoSChiral();
  else if (eosType == 2)
   eos = new EoSAZH();
+ else if (eosType == 3)
+     eos = new EoSCMF();
  else {
   cout << "eosType != 0,1,2\n";
   return 0;
